98bf1dfa8541327d9a195293f75f9facd3f8223b
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2026-05-18 13:52:15 +0700

    Updatez

--- src/components/DelayGTM.tsx
+++ src/components/DelayGTM.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Script from "next/script";
 
 export const DelayGTM = ({ gtmId }: { gtmId: string }) => {
   const [loadGTM, setLoadGTM] = useState(false);
@@ -8,27 +9,28 @@ export const DelayGTM = ({ gtmId }: { gtmId: string }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoadGTM(true);
-    }, 2000);
+    }, 6000);
 
     return () => clearTimeout(timer);
   }, []);
 
+  if (!loadGTM) return null;
+
   return (
     <>
-      {loadGTM && (
-        <script
-          id="gtm-script"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${gtmId}');
-            `
-          }}
-        />
-      )}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');
+          `
+        }}
+      />
       <noscript>
         <iframe
           src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
